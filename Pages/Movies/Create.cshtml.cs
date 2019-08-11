f27d4f27dca267abe78aaae971c0c3158ad24cd6
P0067407 <qingpeng.wang@pactera.com>
2019-08-12 05:48:53 +0800

    推送版本

---
+++ Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using demo.Models;
+
+namespace demo.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly demo.Models.demoContext _context;
+
+        public CreateModel(demo.Models.demoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using demo.Models;
+
+namespace demo.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly demo.Models.demoContext _context;
+
+        public IndexModel(demo.Models.demoContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
